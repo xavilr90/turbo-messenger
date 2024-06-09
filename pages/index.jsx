f9bded051e40aa77e2d113317b7c0b46644493a4
Luis Fernando <fernando201022@hotmail.com>
2024-06-08 20:00:54 -0500

    Adding example of useEffect on index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,21 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch('/api/clientes')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+      })
+  }, [])
+
   return (
     <div>
       <h1>TurboMessenger</h1>
+      <div>{data && data.map(item => <p>{Object.values(item).join(', ')}</p>)}</div>
     </div>
   );
 }
